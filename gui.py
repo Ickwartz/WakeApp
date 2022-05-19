7343026c7664cabe5d2ea9d593ddede04a38e336
Nick Schiffke <Nick.Schiffke@yahoo.com>
2022-05-19 09:25:14 +0200

    Created GUI layout

---
+++ gui.py
@@ -0,0 +1,67 @@
+import imp
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
+def submit_click():
+    msg=f"Time entered: {time_value.get()}"
+    showinfo(
+        title="Time",
+        message=msg
+    )
+
+
+# create Window
+root = tk.Tk()  
+root.geometry("300x500")
+root.resizable(False, False)
+root.title("WakeApp")
+
+
+arrival_time_value = tk.StringVar(value="00:00")
+prep_time_value = tk.StringVar(value="00")
+start_point = tk.StringVar(value="")
+destination = tk.StringVar(value="")
+
+
+# frame for time input
+input_frame = ttk.Frame(root)
+input_frame.pack(padx=10, pady=10, fill="x", expand=True)
+
+
+# time of arrival input
+arrival_label = ttk.Label(input_frame, text="Ankuftszeit: ")
+arrival_label.pack(fill="x", expand=True)
+
+arrival_entry = ttk.Entry(input_frame, textvariable=arrival_time_value)
+arrival_entry.pack(fill="x", expand=True)
+
+
+# time to get ready input
+prep_label = ttk.Label(input_frame, text="Benötigte Zeit zum fertig zu machen (min): ")
+prep_label.pack(fill="x", expand=True)
+
+prep_entry = ttk.Entry(input_frame, textvariable=prep_time_value)
+prep_entry.pack(fill="x", expand=True)
+
+
+# start point input
+start_label = ttk.Label(input_frame, text="Wohnort: ")
+start_label.pack(fill="x", expand=True)
+
+start_entry = ttk.Entry(input_frame, textvariable=start_point)
+start_entry.pack(fill="x", expand=True)
+
+
+# destination input
+destination_label = ttk.Label(input_frame, text="Zielort: ")
+destination_label.pack(fill="x", expand=True)
+
+destination_input = ttk.Entry(input_frame, textvariable=destination)
+destination_input.pack(fill="x", expand=True)
+
+# submit button
+ttk.Button(input_frame, text="berechnen", command=submit_click).pack()
+
+
+root.mainloop()
